8701e8f174a9a9bd972ad54dc7042d84fb09d7b4
Mucsaaa96 <mucsaaa96@gmail.com>
2025-10-18 17:11:16 +0200

    Make function getComputerChoice
    Write a function that randomly returns "rock", "paper" or "scissors".

--- javascript.js
+++ javascript.js
@@ -3,3 +3,14 @@ console.log("Hello,World!");
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
+
+function getComputerChoice() {
+    let computerAnswer = Math.random();
+        if (computerAnswer > 0.66)
+        return rock;
+    else if (computerAnswer > 0.33)
+        return paper;
+    else return scissors;
+}
+
+
